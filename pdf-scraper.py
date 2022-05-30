6fdc1a17eb26d265174242adff6c5fbceb9058e2
njgriffiths <njgriffiths@gmail.com>
2022-05-30 14:54:44 -0700

    renamed init.py. hla outputs csv now

--- pdf-scraper.py
+++ pdf-scraper.py
@@ -6,18 +6,22 @@ import pandas as pd
 file_path = './data/pdf/illicit-drug.pdf'
 file_url = 'https://www2.gov.bc.ca/assets/gov/birth-adoption-death-marriage-and-divorce/deaths/coroners-service/statistical/illicit-drug.pdf'
 
-output_path = './data/deaths-by-lha.csv'
+lha_data = './data/deaths-by-lha.csv'
 
-# read pdf from file
-df = read_pdf(file_path, pages='18-20', output_format="json")
-# convert_into(file_path, output_path, pages='18-20', output_format='csv')
+def scrapeLHA(input, output,):
+    # read LHA table from PDF
+    df = read_pdf(file_path, output_format="dataframe", pages='18-20', pandas_options={'header': None, 'names':['LHA','2016','2017','2018','2019','2020','2021','Deaths this year']}, multiple_tables=True, stream=True, area=[194.6,31,699,572])
+    
+    # drop non-data rows
+    df = df[0].iloc[:-16]
+    print(df)
 
-# df = tabulate(df)
-df = pd.json_normalize(df)
+    # write csv file
+    df.to_csv(lha_data, index=False)
 
-print(df.data)
 
-# write csv file
-df.data.to_csv('./data/deaths-by-lha.csv')
+# AUTOBOTS... ROLL OUT!!!
+scrapeLHA(file_url, lha_data)
+# more scrapers here...
 
 print('DONE!!!')
