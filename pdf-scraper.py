21ec1e1220b4af921bbfca71bd603eed7e59ab8a
njgriffiths <njgriffiths@gmail.com>
2022-05-30 21:37:48 -0700

    lha json test

--- pdf-scraper.py
+++ pdf-scraper.py
@@ -9,13 +9,15 @@ import pandas as pd
 file_path = './data/pdf/illicit-drug.pdf'
 lha_path = './data/source/lha-2018-epsg3857.json'
 lha_data = './data/deaths-by-lha.csv'
-lha_json_path = './data/source/lha-json-test.json'
+lha_json_path = './data/lha-json-test.json'
 file_url = 'https://www2.gov.bc.ca/assets/gov/birth-adoption-death-marriage-and-divorce/deaths/coroners-service/statistical/illicit-drug.pdf'
 
 
 def scrapeLHA(input, output,):
     # lha_json = pd.read_json(lha_path)
     lha_df = gpd.read_file(lha_path)
+
+    # print(lha_df)
 
     # read LHA table from PDF
     df = read_pdf(file_path, output_format="dataframe", pages='18-20', pandas_options={'header': None, 'names':['LHA_NAME','2016','2017','2018','2019','2020','2021','Deaths this year']}, multiple_tables=True, stream=True, area=[194.6,31,699,572])
@@ -25,10 +27,12 @@ def scrapeLHA(input, output,):
     
     # merge with LHA geojson data
     df_geo = lha_df.merge(df, on='LHA_NAME', how='left')
-    print(df_geo)
+    # print(df_geo)
+    
 
     # write csv file
     df_geo.to_csv(lha_data, index=False)
+    df_geo.to_file(lha_json_path, driver='GeoJSON', drop_id=True)
 
 
 # AUTOBOTS... ROLL OUT!!!
