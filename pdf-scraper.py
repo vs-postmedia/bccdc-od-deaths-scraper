f60de2866ea534232b58839720164bd9bcf5e63b
njgriffiths <njgriffiths@gmail.com>
2022-06-02 09:53:05 -0700

    scrape city deaths table

--- pdf-scraper.py
+++ pdf-scraper.py
@@ -9,10 +9,11 @@ import pandas as pd
 file_path = './data/pdf/illicit-drug.pdf'
 lha_path = './data/source/lha-2018-epsg4326_7pct.json'
 lha_json_path = './data/deaths-by-lha.json'
+city_deaths_path = './data/deaths-by-city.csv'
 #  this URL doesn't work for some reason...
 file_url = 'https://www2.gov.bc.ca/assets/gov/birth-adoption-death-marriage-and-divorce/deaths/coroners-service/statistical/illicit-drug.pdf'
 
-
+# FUNCTIONS
 def scrapeLHA(input_file, output_file):
     # lha_json = pd.read_json(lha_path)
     lha_df = gpd.read_file(lha_path)
@@ -31,8 +32,23 @@ def scrapeLHA(input_file, output_file):
     df_geo.to_file(output_file, driver='GeoJSON', drop_id=True)
 
 
+def scrapeCityDeaths(input_file, output_file):
+    # read city deaths table from PDF
+    df = read_pdf(input_file, output_format="dataframe", pages='11', stream=True, area=[130,52.5,424,588])
+
+    # print(df)
+    # drop "total" & "other" rows
+    df = df[0].iloc[:-2]
+    print(df)
+
+    # write csv file
+    df.to_csv(output_file, index=False)
+
+    #  pandas_options={'header': None, 'names':['LHA_NAME','2016','2017','2018','2019','2020','2021','Deaths this year']}, multiple_tables=True, stream=True, area=[194.6,31,699,572])
+
 # AUTOBOTS... ROLL OUT!!!
-scrapeLHA(file_path, lha_json_path)
+# scrapeLHA(file_path, lha_json_path)
+scrapeCityDeaths(file_path, city_deaths_path)
 # more scrapers here...
 
 print('DONE!!!')
