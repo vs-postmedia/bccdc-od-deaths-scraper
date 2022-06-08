1c16ac7dc41c39a019e73f87a006730a8ffd9a5e
njgriffiths <njgriffiths@gmail.com>
2022-06-08 11:40:54 -0700

    lha scraper is missing rows...

--- pdf-scraper.py
+++ pdf-scraper.py
@@ -3,13 +3,15 @@ from tabula import convert_into, read_pdf
 from geopandas import gpd
 # from tabulate import tabulate
 import pandas as pd
-# import json
+import json
+from glom import glom
+
 
 # VARS
 current_year = '2021'
 file_path = './data/pdf/illicit-drug.pdf'
 lha_geo_path = './data/source/lha-2018-epsg4326_7pct.json'
-lha_jcsv_path = './data/deaths-by-lha.csv'
+lha_csv_path = './data/deaths-by-lha.csv'
 lha_json_path = './data/deaths-by-lha.json'
 city_deaths_path = './data/deaths-by-city.csv'
 #  this URL doesn't work for some reason...
@@ -17,20 +19,32 @@ file_url = 'https://www2.gov.bc.ca/assets/gov/birth-adoption-death-marriage-and-
 
 # FUNCTIONS
 def scrapeLHA(input_file, json_output, csv_output):
-    # lha_json = pd.read_json(lha_path)
+    # admin boundaries for LHAs
     lha_df = gpd.read_file(lha_geo_path)
 
-    # read LHA table from PDF
-    df1 = read_pdf(input_file, output_format="dataframe", pages='18-19', pandas_options={'header': None, 'names':['LHA_NAME','2016','2017','2018','2019','2020','2021','Deaths this year']}, stream=True, area=[180,31,725,572])
+    # read LHA tables from PDF
+    dfx = read_pdf(input_file, output_format="json", pages="19", stream=True, area=[180,31,2000,572])
+    
+    # with open('./data/test.json', 'w') as f:
+    #     json.dump(dfx, f)
+
+    # convert json into dataframe
+    df1 = pd.json_normalize(dfx, record_path=['data'][0])
+    print(df1.head(5))
+    
+
+    df1 = read_pdf(input_file, output_format="dataframe", pages='19', pandas_options={'header': None, 'names':['LHA_NAME','2016','2017','2018','2019','2020','2021','Deaths this year']}, stream=True, area=[180,31,2000,572])
     df2 = read_pdf(input_file, output_format="dataframe", pages='20', pandas_options={'header': None, 'names':['LHA_NAME','2016','2017','2018','2019','2020','2021','Deaths this year']}, stream=True, area=[180,31,400,572])
     
     # drop non-data rows
     df1 = df1[0].iloc[:-16]
     df2 = df2[0]
+
+    df1.to_csv('./data/test.csv')
  
     # text cleanup
     df1['LHA_NAME'] = df1['LHA_NAME'].str.replace('Maple Ridge/Pitt', 'Maple Ridge/Pitt Meadows')
-    df2['LHA_NAME'] = df2['LHA_NAME'].str.replace('West Vancouver & Bowen', 'West Vancouver/Bowen Island')
+    df2['LHA_NAME'] = df2['LHA_NAME'].str.replace('West Vancouver/ Bowen', 'West Vancouver & Bowen Island')
 
     df1.drop(index = df1[df1['LHA_NAME'] == 'Meadows'].index, inplace=True)
     df2.drop(index = df2[df2['LHA_NAME'] == 'Island'].index, inplace=True)
@@ -38,8 +52,11 @@ def scrapeLHA(input_file, json_output, csv_output):
     # concat results from all 3 df pages
     df = pd.concat([df1, df2], axis=0)
 
-    # merge with LHA geojson data
-    df_current = df[['LHA_NAME', current_year]]
+    lha = ['Gulf']
+    print(df1[df1['LHA_NAME'].isin(lha)])
+
+    # merge with LHA boundaries
+    df_current = df[['LHA_NAME', current_year, 'Deaths this year']]
     df_geo = lha_df.merge(df_current, on='LHA_NAME', how='left')
     df_geo.fillna('', inplace=True)
 
@@ -65,7 +82,7 @@ def scrapeCityDeaths(input_file, output_file):
 
 
 # AUTOBOTS... ROLL OUT!!!
-scrapeLHA(file_path, lha_json_path, lha_jcsv_path)
+scrapeLHA(file_path, lha_json_path, lha_csv_path)
 # scrapeCityDeaths(file_path, city_deaths_path)
 # more scrapers here...
 
